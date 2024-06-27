e2b1e26bc34281abea3ecd3c24d4bd127c67c3d6
Jangseun <164428037+Jangseun@users.noreply.github.com>
2024-06-27 20:00:38 +0900

    06-27
    ㅎㅎ

--- crud/app.js
+++ crud/app.js
@@ -54,5 +54,31 @@ app.post("/webtoon", (req, res) => {
     res.status(200).json(newItem);
 });
 
+app.delete("/webtoon/:id", (req, res) => {
+    const itemId = parseInt(req.params.id, 10);
+    const itemIndex = webtoon.findIndex((i) => i.id === itemId);
+    if (itemIndex !== -1) {
+        webtoon.splice(itemId, 1);
+        res.status(204).send("데이터 삭제됨");
+    }
+    else {
+        res.status(404).json({ message: "삭제할 데이터가 존재하지 않습니다."});
+    }
+});
+
+
+app.put("/webtoon/:id", (req, res) => {
+    const itemId = parseInt(req.params.id, 10);
+    const itemIndex = webtoon.findIndex((i) => i.id === itemId);
+    if (itemIndex !== -1) {
+        const updatedItem = {...items[itemIndex+1], ...req.body }
+        webtoon[itemIndex+1] = updatedItem;
+        res.json(updatedItem);
+    }
+    else {
+        res.status(404).json({ message: "업데이트 할 데이터가 존재하지 않습니다."});
+    }
+});
+
 
 
